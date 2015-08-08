8a85f1fa5bccff72fd8c7b91ad52790f4755d3c7
luizguilhermecm <luizgui@gmail.com>
2015-08-07 21:16:19 -0300

    bootstrap e todolist

--- kit_todo.rb
+++ kit_todo.rb
@@ -4,20 +4,54 @@ class Kit < Sinatra::Base
       session!
 
       if params[:insert] != nil and params[:insert] != ""
-          last = @@conn.exec_params(' select * from todo_list where id = $1', [params[:insert]])
 
-          @feedback = last.first.to_s
+          query = " SELECT id, text, to_char(created_at, '[DD/MM/YYYY]') as created_at FROM todo_list where id = $1 ";
+          last = @@conn.exec_params(query, [params[:insert]])
+
+          @feedback = []
+
+          last.each_with_index do |t, i|
+              @feedback << {
+                  :id => t["id"],
+                  :text => t["text"],
+                  :created_at => t["created_at"],
+              }
+          end
       end
       erb :todo
   end
 
   get '/new_todo' do
       session!
+
       text = params[:text]
+
       ret = @@conn.exec_params(' INSERT INTO todo_list(text) VALUES($1) returning id', [text.to_s])
+
       id = ret.first
       puts id.to_s
       #redirect "/"
       redirect "/todo?insert=#{id["id"]}"
+  end
+
+  get '/todo_list' do
+
+      query = " SELECT id, text, to_char(created_at, '[DD/MM/YYYY]') as created_at FROM todo_list ";
+      ret = @@conn.exec(query)
+
+      @todos = []
+
+      ret.each_with_index do |t, i|
+
+          @todos << {
+              :id => t["id"],
+              :text => t["text"],
+              :created_at => t["created_at"],
+          }
+          puts t
+
+      end
+
+      erb :todo_list
   end
 end
