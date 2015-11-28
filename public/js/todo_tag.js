d9ddccf2846a48b72fdaa9137c8c8208336e79bc
luizguilhermecm <luizgui@gmail.com>
2015-11-28 10:41:12 -0200

    checkbox checked and not

--- public/js/todo_tag.js
+++ public/js/todo_tag.js
@@ -1,7 +1,13 @@
 
 function update_flag_do_it(flag_do_it_p, todo_id) {
-    var flag_do_it = flag_do_it_p.value;
     var todo_id = todo_id
+
+    if (flag_do_it_p.checked) {
+        var flag_do_it = true;
+    } else {
+        var flag_do_it = false;
+    }
+
 
    $.ajax({
         url: "/update_todo_flag_do_it/",
@@ -13,6 +19,7 @@ function update_flag_do_it(flag_do_it_p, todo_id) {
     }).done( function(res) {
     });
 }
+
 function update_tag_id(tag_id, todo_id) {
     var tag_id = tag_id.value;
     var todo_id = todo_id
