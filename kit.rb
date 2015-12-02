abdbf480d72a58711cb7da14e49d098af8e0f668
luizguilhermecm <luizgui@gmail.com>
2015-12-02 07:48:54 -0200

    indentation

--- kit.rb
+++ kit.rb
@@ -16,115 +16,115 @@ require_relative 'kit_todo'
 require_relative 'bijqc'
 
 class Kit < Sinatra::Base
-  register Sinatra::Session
-  set :bind, '0.0.0.0'
-  set :port, 80 # to deploy on tcp port 80, sudo needed
-  set :environment, :production
-  set :host, DB_HOST
-  set :dbname, DB_NAME
-  set :user, DB_SYS_USER
-  set :password, DB_SYS_PASSWD
+    register Sinatra::Session
+    set :bind, '0.0.0.0'
+    set :port, 80 # to deploy on tcp port 80, sudo needed
+    set :environment, :production
+    set :host, DB_HOST
+    set :dbname, DB_NAME
+    set :user, DB_SYS_USER
+    set :password, DB_SYS_PASSWD
 
-  @@conn = PG.connect(:host => settings.host, :dbname => settings.dbname, :user => settings.user, :password => settings.password)
+    @@conn = PG.connect(:host => settings.host, :dbname => settings.dbname, :user => settings.user, :password => settings.password)
 
-  enable :sessions
+    enable :sessions
 
-  set :session_fail, '/login'
-  set :session_secret, 'whatThisMean?'
+    set :session_fail, '/login'
+    set :session_secret, 'whatThisMean?'
 
 
-  set :dump_errors, false
+    set :dump_errors, false
 
-  configure :production do
-    use Rack::Session::Pool
-    set :erb, :trim => '-'
-  end
+    configure :production do
+        use Rack::Session::Pool
+        set :erb, :trim => '-'
+    end
 
-  not_found do
-      erb :error
-  end
+    not_found do
+        erb :error
+    end
 
-  get '/' do
-      if session?
-          redirect to('/todo')
-      else
-          erb :index
-      end
-  end
-
-  get '/logout' do
-      session_end!
-      redirect to('/')
-  end
-
-  get '/login' do
-      erb :index
-  end
-
-  post '/login' do
-      puts "/login"
-      if session?
-          redirect to('/todo')
-      else
-        query = " SELECT id FROM users WHERE username = $1 AND passwd = $2 "
-        begin
-            ret = @@conn.exec_params(query, [params[:username], params[:passwd]])
-        rescue => e
-            puts "***************************"
-            puts params[:username]
-            puts params[:passwd]
-            puts request.ip
-            puts e
-            puts "***************************"
-            redirect to('/')
-        end
-        puts ret.first.to_s
-        if ret.first
-            session_start!
-            puts "starting session"
-            session[:username] = params[:username]
-            session[:uid] = ret.first["id"]
-            redirect to('/')
+    get '/' do
+        if session?
+            redirect to('/todo')
         else
-            session_start!
-
-            log = "log_error: username: #{params[:username]} passwd: #{params[:passwd]} "
-            log += " with IP: " + request.ip
-            query = " INSERT INTO sys_log (log) VALUES ('#{log}') "
-            @@conn.exec(query)
-
-            session[:username] = params[:username]
-            session[:uid] = 0
-
-            redirect to('/')
-            #erb :error , layout: false
+            erb :index
         end
-        #puts ret.first
+    end
 
-      end
-  end
-
-  get '/git_pull' do
-    session!
-    if session[:uid] != 0
-        `git pull`
+    get '/logout' do
+        session_end!
         redirect to('/')
     end
-  end
 
-  get '/sinatra_log' do
-    session!
-    if session[:uid] != 0
-        #@output = IO.read('nohup.out')
-        @output = `tail -n 50 nohup.out`
-        erb :log
-    else
-        redirect to('/')
+    get '/login' do
+        erb :index
     end
-  end
-  get '/sinatra_log/:filename' do |filename|
-      send_file "./#{filename}", :filename => filename, :type => 'Application/octet-stream'
-  end
 
-  run! if app_file == $0
+    post '/login' do
+        puts "/login"
+        if session?
+            redirect to('/todo')
+        else
+            query = " SELECT id FROM users WHERE username = $1 AND passwd = $2 "
+            begin
+                ret = @@conn.exec_params(query, [params[:username], params[:passwd]])
+            rescue => e
+                puts "***************************"
+                puts params[:username]
+                puts params[:passwd]
+                puts request.ip
+                puts e
+                puts "***************************"
+                redirect to('/')
+            end
+            puts ret.first.to_s
+            if ret.first
+                session_start!
+                puts "starting session"
+                session[:username] = params[:username]
+                session[:uid] = ret.first["id"]
+                redirect to('/')
+            else
+                session_start!
+
+                log = "log_error: username: #{params[:username]} passwd: #{params[:passwd]} "
+                log += " with IP: " + request.ip
+                query = " INSERT INTO sys_log (log) VALUES ('#{log}') "
+                @@conn.exec(query)
+
+                session[:username] = params[:username]
+                session[:uid] = 0
+
+                redirect to('/')
+                #erb :error , layout: false
+            end
+            #puts ret.first
+
+        end
+    end
+
+    get '/git_pull' do
+        session!
+        if session[:uid] != 0
+            `git pull`
+            redirect to('/')
+        end
+    end
+
+    get '/sinatra_log' do
+        session!
+        if session[:uid] != 0
+            #@output = IO.read('nohup.out')
+            @output = `tail -n 50 nohup.out`
+            erb :log
+        else
+            redirect to('/')
+        end
+    end
+    get '/sinatra_log/:filename' do |filename|
+        send_file "./#{filename}", :filename => filename, :type => 'Application/octet-stream'
+    end
+
+    run! if app_file == $0
 end
