920abfc82604f83b9e0c661bb8ab13a52f6222e6
luizguilhermecm <luizgui@gmail.com>
2016-01-04 00:26:10 -0200

    dir per user

--- kit_files.rb
+++ kit_files.rb
@@ -12,12 +12,13 @@ class Kit < Sinatra::Base
         session!
         kit_log(KIT_LOG_VERBOSE, 'uploading file', params['file'])
         begin
-            if File.exist?('uploads/' + params['file'][:filename])
-                File.open('uploads/X' + params['file'][:filename], "w") do |f|
+            check_directory_exists
+            if File.exist?("uploads/#{session[:username]}/" + params['file'][:filename])
+                File.open("uploads/#{session[:username]}/X" + params['file'][:filename], "w") do |f|
                     f.write(params['file'][:tempfile].read)
                 end
             else
-            File.open('uploads/' + params['file'][:filename], "w") do |f|
+            File.open("uploads/#{session[:username]}/" + params['file'][:filename], "w") do |f|
                 f.write(params['file'][:tempfile].read)
             end
             end
@@ -28,24 +29,48 @@ class Kit < Sinatra::Base
         redirect to('/kit_files')
     end
 
+    def check_directory_exists
+        kit_log(KIT_LOG_INFO, 'def check_directory_exists')
+
+        if File.directory?("./uploads/#{session[:username]}")
+            kit_log(KIT_LOG_DEBUG, 'directory exists', "./uploads/#{session[:username]}")
+        else
+            create_user_directory
+        end
+    end
+
+    def create_user_directory
+        kit_log(KIT_LOG_INFO, 'def create_user_directory', "./uploads/#{session[:username]}")
+        begin
+            Dir.mkdir("./uploads/#{session[:username]}")
+        rescue => e
+            kit_log(KIT_LOG_PANIC,
+                    "[ERROR] while creating a new directory for user",
+                    e, session)
+            redirect to('/kit_files')
+        end
+    end
+
     def list_uploaded_files
         kit_log(KIT_LOG_INFO, 'def list_uploaded_files')
-        session!
-        files = Dir.glob("./uploads/*").map{|f| f.split('/').last}
-        kit_log(KIT_LOG_DEBUG, 'list of files in ./uploads', files)
+
+        files = Dir.glob("./uploads/#{session[:username]}/*").map{|f| f.split('/').last}
+        kit_log(KIT_LOG_DEBUG, "list of files in ./uploads/#{session[:username]}", files)
         return files
     end
 
     get '/kit_files/download/:filename' do |filename|
         kit_log(KIT_LOG_INFO, 'get /kit_files/download/:filename', filename)
         session!
-        send_file "./uploads/#{filename}", :filename => filename, :type => 'Application/octet-stream'
+        send_file "./uploads/#{session[:username]}/#{filename}",
+            :filename => filename, :type => 'Application/octet-stream'
     end
 
     get '/kit_files/delete/:filename' do |filename|
         kit_log(KIT_LOG_INFO, 'get /kit_files/delete/:filename', filename)
         session!
-        File.rename("./uploads/#{filename}", "./uploads/.#{filename}")
+        File.rename("./uploads/#{session[:username]}/#{filename}",
+                    "./uploads/.#{filename}")
         redirect to('/kit_files')
     end
 
